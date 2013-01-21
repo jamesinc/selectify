09cffafb02c12b95bb305c269eeb4a91813ec4a4
James Ducker <james.ducker@gmail.com>
2013-01-21 11:21:23 +1100

    Added lots of comments

--- jquery.selectify.js
+++ jquery.selectify.js
@@ -1,7 +1,19 @@
+/**
+ * Selectify, an angry jQuery plugin
+ * by James Ducker <james.ducker+js@gmail.com>
+ * 
+ * Works with jQuery >= 1.7
+ *
+ * Enhances <select> elements with a custom UI.
+ *
+ * Project home: https://github.com/jamesinc/selectify
+ * Issue tracker: https://github.com/jamesinc/selectify/issues
+ */
 ( function ( $ ) {
 
 	"use strict";
 
+	// Got some default options up in here.
 	var defaults = {
 		duration: 50,
 		classes: {
@@ -13,34 +25,66 @@
 		}
 	};
 
+	/**
+	 * Call against any select element or collection containing select elements
+	 * Non-select elements will be ignored.
+	 * @param  {Object} options Options to pass to the plugin.
+	 *                          Valid options are:
+	 *                          duration	(number, milliseconds) slide up/slide
+	 *                                      down animation duration.
+	 *                          classes.container
+	 *                                      Class name of the container element
+	 *                          classes.placeholder
+	 *                                      Class name of the placeholder element
+	 *                                      (holds the current value of the select)
+	 *                          classes.options
+	 *                                      Class name of the dropdown
+	 *                                      container <ul> element
+	 *                          classes.selected
+	 *                                      Class name of the option that is currently
+	 *                                      selected. Applied to <a> inside <li>
+	 *                                      inside options <ul>.
+	 *                          classes.open
+	 *                                      Added to container element when the
+	 *                                      options list is open.
+	 *                          
+	 * @return {jQuery}         The jQuery object.
+	 */
 	$.fn.selectify = function ( options ) {
 
 		var settings;
 
 		options = options || { };
 
+		// Merge defaults and options to make settings
 		settings = $.extend( true, { }, defaults, options );
 
+		// Go through the jQuery collection
 		this.each( function ( ) {
 
 			var el = $( this ),
 				options,
 				container, placeholder, list, anchors;
 
-			// If it's not a select element, skip it.
+			// If current element is not a select element, skip it.
 			if ( !el.is("select") ) {
 
 				return;
 
 			}
 
+			// Check to see if this element has already been selectified
 			if ( el.data("sl") === true ) {
 
+				// Remove old selectifications from the element
 				el.next( "." + settings.classes.container ).remove();
 
 			}
 
+			// Get options list
 			options = el.find( "option" );
+
+			// Create container element for the new UI
 			container = $( "<div/>", {
 				"class": settings.classes.container,
 				css: {
@@ -48,10 +92,13 @@
 				}
 			});
 
+			// Create placeholder element to display the select's
+			// current value
 			placeholder = $( "<div/>", {
 				"class": settings.classes.placeholder
 			});
 
+			// Create the list element for options
 			list = $( "<ul/>", {
 				"class": settings.classes.options,
 				css: {
@@ -60,6 +107,7 @@
 				}
 			});
 
+			// Go through each option and add it to the list
 			options.each( function ( ) {
 
 				var self = $( this ),
@@ -97,6 +145,7 @@
 
 			});
 
+			// Handle list open/close.
 			placeholder.on( "click", function ( e ) {
 
 				e.preventDefault();
@@ -106,6 +155,7 @@
 
 			});
 
+			// Close the list if the user clicks elsewhere on the page.
 			$( document ).on( "mouseup", function ( e ) {
 
 				if ( !container.has( e.target ).length ) {
@@ -118,7 +168,7 @@
 			});
 
 			// Listen for change events on the original select element
-			el.on("change", function ( e, src ) {
+			el.on( "change", function ( e, src ) {
 
 				var index = this.selectedIndex;
 
@@ -156,12 +206,11 @@
 			// Add a height offset to the list
 			list
 				.hide()
-				.css({
-						top: container.outerHeight()
-				});
+				.css( "top", container.outerHeight() );
 
 		});
 
+		// return the jQuery object
 		return this;
 
 	};
